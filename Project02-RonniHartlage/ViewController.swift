a2caddabed65657106e1203927f8885db9117ad2
Ronni Hartlage <hartlage.7@wright.edu>
2020-11-11 11:32:31 -0500

    Initial Commit

---
+++ Project02-RonniHartlage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project02-RonniHartlage
+//
+//  Created by Ronni Hartlage on 11/11/20.
+//  Copyright © 2020 Ronni Hartlage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
